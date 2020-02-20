6e217c3f9353edcb025060dc32c19a45efcb5ab4
Stefano Ortona <stefano.ortona@meltwater.com>
2020-02-20 19:01:59 +0000

    Fix bug

--- src/main/java/com/google/fantasticgeneration/hashcode_2020/logic/ProblemSolver.java
+++ src/main/java/com/google/fantasticgeneration/hashcode_2020/logic/ProblemSolver.java
@@ -1,57 +1,61 @@
 package com.google.fantasticgeneration.hashcode_2020.logic;
 
-import com.google.fantasticgeneration.hashcode_2020.model.*;
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import com.google.fantasticgeneration.hashcode_2020.model.Book;
+import com.google.fantasticgeneration.hashcode_2020.model.Library;
+import com.google.fantasticgeneration.hashcode_2020.model.ProblemContainer;
+import com.google.fantasticgeneration.hashcode_2020.model.SolutionContainer;
+import com.google.fantasticgeneration.hashcode_2020.model.Status;
 
 public class ProblemSolver {
-    private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
+	private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
 
-    private static PickNextLibrary LIBRARY_PICKER = new PickBasedOnScore();
+	private static PickNextLibrary LIBRARY_PICKER = new PickBasedOnScore();
 
-    public SolutionContainer solve(ProblemContainer problem) {
-        Status status = problem.status;
+	public SolutionContainer solve(ProblemContainer problem) {
+		final Status status = problem.status;
 
-        for (int currentTime = 0; currentTime < status.getMaxDays(); ) {
+		for (int currentTime = 0; currentTime < status.getMaxDays();) {
 
-            Library nextLibrary = LIBRARY_PICKER.pickNext(status, currentTime);
+			final Library nextLibrary = LIBRARY_PICKER.pickNext(status, currentTime);
 
-            if (nextLibrary == null) {
-                break; // SKIP: no more libraries
-            }
+			if (nextLibrary == null) {
+				break; // SKIP: no more libraries
+			}
 
-            nextLibrary.setSignupDay(currentTime);
-            List<Book> deliveredBooks = nextLibrary.deliver(status.getDeliveredBooks(), currentTime, status.getMaxDays());
+			nextLibrary.setSignupDay(currentTime);
+			final List<Book> deliveredBooks = nextLibrary.deliver(status.getDeliveredBooks(), currentTime,
+					status.getMaxDays());
 
-            nextLibrary.setDeliveredBooks(deliveredBooks);
-            status.addDeliveredBooks(deliveredBooks);
+			nextLibrary.setDeliveredBooks(deliveredBooks);
+			status.addDeliveredBooks(deliveredBooks);
 
-            currentTime += nextLibrary.getSignupTime();
-        }
+			currentTime += nextLibrary.getSignupTime();
+		}
 
-        int score = calculateScore(status.getLibraries());
+		final int score = calculateScore(status.getDeliveredBooks());
 
-        SolutionContainer solutionContainer = new SolutionContainer(status.getLibraries(), score);
-        return solutionContainer;
-    }
+		final SolutionContainer solutionContainer = new SolutionContainer(status.getLibraries(), score);
+		return solutionContainer;
+	}
 
+	/*
+	 * Internal methods
+	 */
 
-    /*
-     * Internal methods
-     */
+	private int calculateScore(Set<Book> books) {
+		int score = 0;
 
-    private int calculateScore(List<Library> libraries) {
-        int score = 0;
+		for (final Book book : books) {
+			score += book.getScore();
+		}
 
-        for (Library library : libraries) {
-            for (Book book : library.getBooks()) {
-                score += book.getScore();
-            }
-        }
-
-        return score;
-    }
+		return score;
+	}
 
 }
