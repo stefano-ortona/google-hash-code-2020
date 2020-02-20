d6081eb25e5fb8d234ed0659dab1f58df9470570
carlo luchessa <c.luchessa@reply.it>
2020-02-20 20:15:10 +0100

    up.

--- src/main/java/com/google/fantasticgeneration/hashcode_2020/logic/PickBasedOnScorePesata.java
+++ src/main/java/com/google/fantasticgeneration/hashcode_2020/logic/PickBasedOnScorePesata.java
@@ -1,17 +1,16 @@
 package com.google.fantasticgeneration.hashcode_2020.logic;
 
+import com.google.fantasticgeneration.hashcode_2020.model.Book;
+import com.google.fantasticgeneration.hashcode_2020.model.Library;
+import com.google.fantasticgeneration.hashcode_2020.model.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.fantasticgeneration.hashcode_2020.model.Book;
-import com.google.fantasticgeneration.hashcode_2020.model.Library;
-import com.google.fantasticgeneration.hashcode_2020.model.Status;
-
-public class PickBasedOnScore extends PickNextLibrary {
+public class PickBasedOnScorePesata extends PickNextLibrary {
 
 	final Logger LOG = LoggerFactory.getLogger(getClass());
 
@@ -19,14 +18,15 @@ public class PickBasedOnScore extends PickNextLibrary {
 	public Library pickNext(Status status, int curTime) {
 		final List<Library> available = status.getLibraries().stream().filter(l -> l.getSignupDay() == -1)
 				.collect(Collectors.toList());
-		int bestScore = 0;
+		int bestScore = -1;
 		Library bestLibrary = null;
+
 		for (final Library one : available) {
 			final int curScore = computeScore(one, status.getDeliveredBooks(), curTime, status.getMaxDays());
 			if (curScore > bestScore) {
 				bestScore = curScore;
 				bestLibrary = one;
-			} else if ((curScore > 0) && (curScore == bestScore)) {
+			} else if (curScore == bestScore) {
 				if (one.getSignupTime() < bestLibrary.getSignupTime()) {
 					bestLibrary = one;
 				}
@@ -35,5 +35,7 @@ public class PickBasedOnScore extends PickNextLibrary {
 		LOG.info("At time '{}' we picked library '{}' with score '{}'", curTime, bestLibrary, bestScore);
 		return bestLibrary;
 	}
+
+
 
 }
--- src/main/java/com/google/fantasticgeneration/hashcode_2020/logic/PickNextLibrary.java
+++ src/main/java/com/google/fantasticgeneration/hashcode_2020/logic/PickNextLibrary.java
@@ -1,13 +1,25 @@
 package com.google.fantasticgeneration.hashcode_2020.logic;
 
+import com.google.fantasticgeneration.hashcode_2020.model.Book;
 import com.google.fantasticgeneration.hashcode_2020.model.Library;
 import com.google.fantasticgeneration.hashcode_2020.model.Status;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Set;
 
-public interface PickNextLibrary {
+public abstract class PickNextLibrary {
 
 	@Nullable
-	public Library pickNext(Status status, int curTime);
+	public abstract Library pickNext(Status status, int curTime);
+
+	public int computeScore(Library l, Set<Book> delivered, int curTime, int maxTime) {
+		final List<Book> possibleDelvier = l.deliver(delivered, curTime, maxTime);
+		int tot = 0;
+		for (final Book oneB : possibleDelvier) {
+			tot += oneB.getScore();
+		}
+		return tot;
+	}
 
 }
